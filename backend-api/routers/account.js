5e9040612c32a5f9ff15145808bb5268a8551007
tycrek <josh.moore@jmoore.dev>
2019-09-16 14:18:39 -0600

    Added create account function

--- backend-api/routers/account.js
+++ backend-api/routers/account.js
@@ -1,8 +1,35 @@
 var router = require('express').Router();
+var psql = require('../sql/psql');
+var Uuid = require('../tools/uuid');
+var password = require('../tools/password');
+var bcrypt = require('../tools/bcrypt');
 
-router.get('/create', (req, res) => {
-	console.log('account create');
-	res.status(200).send('create!');
+// jacob15, 1234WorDA6789!@
+router.get('/create/:name/:pass', (req, res) => {
+	let name = req.params.name;
+	let pass = req.params.pass;
+	let uuid = Uuid.generate();
+
+	_createAccount(name, pass, uuid).then(() => {
+		console.log(`User ${name} created`);
+		res.status(200).send('Created');
+	}).catch((err) => {
+		console.error(err);
+		res.status(200).send(err);
+	});
+
+	function _createAccount(name, pass, uuid) {
+		return new Promise((resolve, reject) => {
+			if (!password.checkRequirements(pass)) {
+				return reject('Bad password');
+			}
+			bcrypt.hash(pass).then((hash) => {
+				psql.createUser(name, uuid, hash).then(() => {
+					resolve();
+				}).catch((err) => reject(err));
+			}).catch((err) => reject(err));
+		});
+	}
 });
 
 module.exports = router;
