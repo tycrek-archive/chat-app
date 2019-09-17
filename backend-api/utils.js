cd0181261d2a3f7c7388a6b85d9fa994bc6a02be
tycrek <josh.moore@jmoore.dev>
2019-09-17 16:30:24 -0600

    Added base64 conversion

--- backend-api/utils.js
+++ backend-api/utils.js
@@ -41,3 +41,6 @@ exports.init = () => {
 exports.getPath = (filename) => {
 	return require('path').join(__dirname, filename);
 }
+
+exports.str2b64 = (str) => Buffer.from(str).toString('base64');
+exports.b642str = (str) => Buffer.from(str, 'base64').toString();
