19fbfd1e76f70d64ce7dda822b2f9c6676b546c1
tycrek <josh.moore@jmoore.dev>
2019-09-17 10:31:27 -0600

    SQL files are now read into RAM

--- backend-api/sql/psql.js
+++ backend-api/sql/psql.js
@@ -3,18 +3,65 @@ var fse = require('fs-extra');
 const { Pool, Client } = require('pg');
 
 var pool;
+var QUERIES = {};
 
 exports.init = () => {
 	return new Promise((resolve, reject) => {
-		fse.readJson(utils.getPath('sql/auth.json'), (err, obj) => {
-			if (err) return reject(err);
-
-			pool = new Pool({
-				connectionString: obj.connectionString
-			});
-			resolve();
-		});
+		_connect().then(() => {
+			_loadQueries().then(() => {
+				resolve();
+			}).catch((err) => reject(err));
+		}).catch((err) => reject(err));
 	});
+
+	function _connect() {
+		return new Promise((resolve, reject) => {
+			fse.readJson(utils.getPath('sql/auth.json'), (err, obj) => {
+				if (err) return reject(err);
+				pool = new Pool({
+					connectionString: obj.connectionString
+				});
+				resolve();
+			});
+		});
+	}
+
+	function _loadQueries() {
+		return new Promise((resolve, reject) => {
+			let total = 0;
+			let count = 0;
+
+			_iterateQueries(resolve, reject, _incrementTotal);
+			_iterateQueries(resolve, reject, _readQuery);
+
+			function _iterateQueries(resolve, reject, task) {
+				let queries = utils.config().queries;
+				queries.segments.forEach((segment) => {
+					Object.keys(queries[segment]).forEach((key) => {
+						let value = queries[segment][key];
+						task(segment, key, value, resolve, reject);
+					});
+				});
+			}
+
+			function _incrementTotal() {
+				total++;
+			}
+
+			function _readQuery(segment, key, value, resolve, reject) {
+				let sqlPath = 'sql/queries/';
+				let fullPath = sqlPath + value;
+
+				fse.readFile(utils.getPath(fullPath), (err, data) => {
+					if (err) return reject(err);
+					if (!QUERIES.hasOwnProperty(segment)) QUERIES[segment] = {};
+					QUERIES[segment][key] = data.toString();
+					count++;
+					if (count === total) resolve();
+				});
+			}
+		});
+	}
 }
 
 exports.createUser = (name, uuid, hash) => {
--- backend-api/sql/queries/getHash.sql
+++
@@ -1,1 +0,0 @@
-SELECT hash FROM users WHERE name LIKE $1;
