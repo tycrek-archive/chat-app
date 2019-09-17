17692968bdf34ee38c61595cbb736931baeffc08
tycrek <josh.moore@jmoore.dev>
2019-09-17 16:30:30 -0600

    Added base64 test

--- backend-api/tests.js
+++ backend-api/tests.js
@@ -1,12 +1,14 @@
 let psql = require('./sql/psql');
 var uuid = require('./tools/uuid');
 var bcrypt = require('./tools/bcrypt');
+var utils = require('./utils');
 
 exports.test = () => {
 	//this.bcrypt();
 	//this.uuid();
 	//this.createUser();
 	//this.testUser();
+	//this.b64();
 }
 
 exports.bcrypt = () => {
@@ -41,4 +43,10 @@ exports.testUser = () => {
 			console.log(same);
 		}).catch((err) => console.error(err));
 	}).catch((err) => console.error(err));
+}
+
+exports.b64 = () => {
+	let foo = utils.str2b64('hello');
+	let bar = utils.b642str(foo);
+	console.log(foo + ' ' + bar);
 }
