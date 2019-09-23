69b4c6a243c70a70f4772c6c83d933f5fc59e91e
tycrek <josh.moore@jmoore.dev>
2019-09-23 08:22:37 -0600

    Switched to buildResponse

--- backend-api/routers/user.js
+++ backend-api/routers/user.js
@@ -21,7 +21,7 @@ router.get('/create/:name/:pass', (req, res) => {
 			.then(() => utils.respond( utils.config().response.success))
 			.catch((err) => {
 				if (err == errorResponse) utils.respond(res, errorResponse);
-				else utils.respond(res, (errorResponse.data = { err: err.toString() }, errorResponse));
+				else utils.respond(res, utils.buildResponse(errorResponse, { err: err.toString() }));
 			});
 	} else {
 		utils.respond(res, errorResponse);
