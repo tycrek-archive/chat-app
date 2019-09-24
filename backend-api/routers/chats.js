1c0f2868e154576ef87ac7e891b8d460db06a611
tycrek <josh.moore@jmoore.dev>
2019-09-24 15:12:45 -0600

    Added chats functions

--- backend-api/routers/chats.js
+++ backend-api/routers/chats.js
@@ -1,2 +1,42 @@
 var router = require('express').Router();
+var Psql = require('../sql/psql');
+var Crypto = require('../crypto');
+var Utils = require('../utils');
+
+router.get('/create/:recipientName', (req, res) => {
+	let token = req.query.token;
+	let recipientName = req.params.recipientName;
+
+	let chatId, senderId, recipientId;
+	Psql.sessionGet(token)
+		.then((dataset) => dataset[0].user_uuid)
+		.then((mSenderId) => senderId = mSenderId)
+		.then(() => Psql.userInfo(true, recipientName))
+		.then((dataset) => dataset[0].uuid)
+		.then((mRecipientId) => recipientId = mRecipientId)
+		.then(() => Crypto.generateUuid())
+		.then((mChatId) => chatId = mChatId)
+		.then(() => Psql.chatsCreate(chatId, senderId, recipientId))
+		.then(() => {
+			let template = Utils.config().response.success;
+			let response = Utils.buildResponse(template, { chatId: chatId });
+			Utils.respond(res, response);
+		})
+		.catch((err), Utils.respond(res, err));
+});
+
+router.get('/list', (req, res) => {
+	let token = req.query.token;
+
+	Psql.sessionGet(token)
+		.then((dataset) => dataset[0].user_uuid)
+		.then((userId) => Psql.chatsList(userId))
+		.then((dataset) => {
+			let template = Utils.config().response.success;
+			let response = Utils.buildResponse(template, { chats: dataset });
+			Utils.respond(res, response);
+		})
+		.catch((err) => Utils.respond(res, err));
+});
+
 module.exports = router;
