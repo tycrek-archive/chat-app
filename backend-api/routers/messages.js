01d579d623d45a91acd9f1fe90cd5c04b24acd70
tycrek <josh.moore@jmoore.dev>
2019-09-24 15:13:02 -0600

    Added messages create function

--- backend-api/routers/messages.js
+++ backend-api/routers/messages.js
@@ -1,2 +1,29 @@
 var router = require('express').Router();
+var Psql = require('../sql/psql');
+var Crypto = require('../crypto');
+var Utils = require('../utils');
+
+router.get('/create/:chatId/:data', (req, res) => {
+	let token = req.query.token;
+	let chatId = req.params.chatId;
+	let data = req.params.data;
+
+	let messageId, timestamp, senderId, recipientId;
+	Psql.chatsGet(chatId)
+		.then((dataset) => dataset[0])
+		.then((chat) => {
+			senderId = chat.sender_id;
+			recipientId = chat.recipient_id;
+			timestamp = Utils.utcStamp();
+			messageId = Crypto.generateUuid();
+		})
+		.then(() => Psql.messagesCreate(messageId, chatId, data, timestamp, senderId, recipientId))
+		.then(() => Utils.respond(res, Utils.config().response.success))
+		.catch((err) => {
+			let template = Utils.config().response.error;
+			let response = Utils.buildResponse(template, { err: err });
+			Utils.send(res, response);
+		});
+});
+
 module.exports = router;
