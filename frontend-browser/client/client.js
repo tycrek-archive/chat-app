f71f597ee263875ab47566002055c662441510cc
tycrek <josh.moore@jmoore.dev>
2019-09-25 10:06:47 -0600

    Moved encryption to crypto.js

--- frontend-browser/client/client.js
+++ frontend-browser/client/client.js
@@ -1,21 +1,4 @@
-var crypto = require('crypto');
-
 const SERVER = 'localhost';
-
-window.encrypt = function (data, publicKey) {
-	let buffer = Buffer.from(data);
-	let encrypted = crypto.publicEncrypt(publicKey, buffer);
-	return encrypted.toString('base64');
-};
-
-window.decrypt = function (data, privateKey, password) {
-	let buffer = Buffer.from(data, 'base64');
-	let decrypted = crypto.privateDecrypt({
-		key: privateKey,
-		passphrase: password
-	}, buffer);
-	return decrypted.toString('utf8');
-};
 
 window.signUp = function () {
 	$('#loading').show();
