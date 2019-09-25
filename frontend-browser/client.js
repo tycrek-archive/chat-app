c9ceecf685c4ed0690c383826c0191089583fe64
tycrek <josh.moore@jmoore.dev>
2019-09-24 18:31:29 -0600

    Added login and chat features

--- frontend-browser/client.js
+++ frontend-browser/client.js
@@ -40,7 +40,7 @@ window.signUp = function () {
 	let username = $('#username').val();
 	let password = btoa($('#password').val());
 
-	fetch(`http://${SERVER}:34682/user/create/${username}/${password}`)
+	this.fetch(`http://${SERVER}:34682/user/create/${username}/${password}`)
 		.then((res) => res.json())
 		.then((json) => {
 			$('#loading').hide();
@@ -49,3 +49,78 @@ window.signUp = function () {
 		})
 		.catch((err) => console.error(err));
 };
+
+window.login = function () {
+	$('#loading').show();
+	let username = $('#username').val();
+	let password = btoa($('#password').val());
+
+	this.fetch(`http://${SERVER}:34682/user/login/${username}/${password}`)
+		.then((res) => res.json())
+		.then((json) => {
+			$('#loading').hide();
+			if (json.code != 200) this.alert(json.reason);
+			else {
+				let token = json.data.token;
+				console.log(token);
+				Cookies.set('token', token, { expires: 7 });
+				pageChats();
+			}
+		});
+}
+
+window.listChats = function () {
+	let token = Cookies.get('token');
+	if (token == null) alert('Please sign in');
+	else {
+		this.fetch(`http://${SERVER}:34682/chats/list?token=${token}`)
+			.then((res) => res.json())
+			.then((json) => {
+				$('#loading').hide();
+				if (json.code != 200) this.alert(json.reason);
+				else {
+					$('#chat-list').html(JSON.stringify(json.data.chats, null, 4));
+				}
+			});
+	}
+}
+
+window.createChat = function () {
+	let token = Cookies.get('token');
+	if (token == null) alert('Please sign in');
+	else {
+		let recipient = $('#recipient').val();
+
+		this.fetch(`http://${SERVER}:34682/chats/create/${recipient}?token=${token}`)
+			.then((res) => res.json())
+			.then((json) => {
+				if (json.code != 200) this.alert(json.reason);
+				else return json.data.chatId;
+			})
+			.then((chatId) => sendMessage(chatId));
+	}
+}
+
+window.sendMessage = function (chatId) {
+	let token = Cookies.get('token');
+	if (token == null) alert('Please sign in');
+	else {
+		let message = $('#message').val();
+		let recipient = $('#recipient').val();
+
+		this.fetch(`http://${SERVER}:34682/keypairs/public/${recipient}?token=${token}`)
+			.then((res) => res.json())
+			.then((json) => {
+				if (json.code != 200) this.alert(json.reason);
+				else return json.data.pubKey;
+			})
+			.then((pubKey) => encrypt(message, pubKey))
+			.then((encrypted) => this.btoa(encrypted))
+			.then((encoded) => this.fetch(`http://${SERVER}:34682/messages/create/${chatId}/${encoded}?token=${token}`))
+			.then((res) => res.json())
+			.then((json) => {
+				if (json.code != 200) this.alert(json.reason);
+				else alert(json.reason);
+		})
+	}
+}
