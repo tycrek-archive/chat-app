a711ef8ae1bff9ac76a74bb885e7ca2605e84ea6
tycrek <josh.moore@jmoore.dev>
2019-09-28 14:43:52 -0600

    Changed chats list again

--- backend-api/sql/queries/chats.list.sql
+++ backend-api/sql/queries/chats.list.sql
@@ -1,12 +1,1 @@
-SELECT
-	U.username
-FROM
-	users U
-INNER JOIN chats C
-	ON U.userid = C.usera
-	OR U.userid = C.userb
-
-WHERE U.userid != $1
-;
-
--- SELECT * FROM chats WHERE usera = $1 OR userb = $2;
+SELECT * FROM chats WHERE usera = $1 OR userb = $2;
